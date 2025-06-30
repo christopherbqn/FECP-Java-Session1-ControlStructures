54bc3c357423d35643ca4f6223a6e12806051c96
christopher.bayquen <christopher.bayquen@gcash.com>
2025-06-30 16:04:15 +0800

    Final Submission, Lab 2 Membership Calculator

---
+++ Readme.md
@@ -0,0 +1,19 @@
+# Lab 2: Membership Gym Price Calculator
+
+Submitted by: Christopher Bayquen | June 30, 2025
+
+This repository holds our submission for our Second Lab Activity in our Java Fundmentals Session. This activity tasked us to create a simple Gym Price calculator given a few user input parameters: Gym Membership Status and Age.
+
+
+### Input/Output :
+
+![Sample Input/Output Screenshot](Sample-Input-Output.png)
+
+### Output Test Validation :
+
+![Sample Output Test Validation](Sample-Output-Test-Validation.png)
+### Sample Output :
+
+![Sample Output Screenshot](Sample-Output.png)
+
+
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,61 @@
 package org.example;
+import java.sql.SQLOutput;
 import java.util.*;
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter membership status (Regular, VIP, Premium):");
+        String membershipStatus = scanner.nextLine();
+        membershipStatus = membershipStatus.toLowerCase();
+
+        // check if membership status is valid
+        if (!membershipStatus.equals("regular") && !membershipStatus.equals("vip") && !membershipStatus.equals("premium")) {
+            System.out.println("Invalid membership status entered.");
+        } else {
+            System.out.print("Enter age: ");
+            int memberAge = scanner.nextInt();
+
+            // calling the calculator method
+            calculatePrice(membershipStatus, memberAge);
+        }
     }
+
+    // calculate membership price
+        public static void calculatePrice(String membershipStatus, int memberAge){
+            float price;
+            switch(membershipStatus){
+                case("regular"):
+                    if(memberAge<18){
+                        price = 50;
+                    } else if(memberAge<=64) {
+                        price = 100;
+                    } else {
+                        price = 75;
+                    }
+                    break;
+                case("vip"):
+                    if(memberAge<18){
+                        price = 75;
+                    } else if(memberAge<=64) {
+                        price = 150;
+                    } else {
+                        price = 112.50f;
+                    }
+                    break;
+                case("premium"):
+                    if(memberAge<18){
+                        price = 100;
+                    } else if(memberAge<=64) {
+                        price = 200;
+                    } else {
+                        price = 150;
+                    }
+                    break;
+                default:
+                    return;
+            }
+            //printing results
+            System.out.printf("Price: $%.2f", price);
+        }
 }
